c086be7410d6cccf5df148489e29e1207aa02911
Allan <allanhender@gmail.com>
2025-11-27 17:25:49 +0000

    working on App.js

--- src/app/App.js
+++ src/app/App.js
@@ -9,23 +9,26 @@ import Author from "../components/Author";
 import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
+import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
 
-import { Route } from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
+const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
+    <Route path="/about/:name" element={ <About/> } />
+    <Route path="/about" element={ <About/> } />
+    <Route path="/sign-up" element={ <SignUp/> } />
+    <Route path="/articles/:title" element={ <Articles/> } />
+    <Route path="/articles" element={ <Articles/> } />
+    <Route path="/categories" element={ <Categories/> } />
+    <Route path="/profile" element={ <Profile/> } />
   </Route>
+));
 
 function App() {
   return (
-    <>
-      { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
-    </>
+      <RouterProvider router={ router } />
   );
 }
 
