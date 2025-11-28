243730c40831ed2d8fb9836081d98c42937a54f4
Allan <allanhender@gmail.com>
2025-11-28 20:27:34 +0000

    working on articles and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
 
-  // get the navigate function
   const navigate = useNavigate();
 
   const goBack = (e) => {
-    // imperatively redirect back
     e.preventDefault();
     navigate(-1);
     
   }
 
   const goForward = (e) => {
-    // imperatively redirect forward
     e.preventDefault();
     navigate(1);
     
