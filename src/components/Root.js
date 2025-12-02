c8e2ce9870a18281efc6324c1d07c7e95798d134
Allan <allanhender@gmail.com>
2025-12-02 21:15:31 +0000

    adding root and signup components

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
+
 export default function Root() {
     return (
         <>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,13 +8,11 @@ export default function SignUp () {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
 
-   // Grab the navigate function
   const navigate = useNavigate();
 
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(signUp({username: username}));
-    // imperatively redirect the user to /profile
     navigate('/profile');
   }
 
